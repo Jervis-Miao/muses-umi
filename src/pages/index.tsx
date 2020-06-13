34e7c78591fcc3a92408698251b8e87a967fb417
jervis <jervis_qiang@aliyun.com>
2020-06-13 19:54:57 +0800

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.less';
+
+export default () => {
+  return (
+    <A
+    <div>
+      <h1 className={styles.title}>Page index11111111111</h1>
+    </div>
+  );
+}
